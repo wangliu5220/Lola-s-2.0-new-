38bf3882bc3a8edab993549ecf0bc98d7e852485
boluwatifeda <bdadeshina1@gmail.com>
2025-11-16 18:54:39 -0500

    Altered Qdrant Model fields

--- Team_M/backend/models/qdrant_models.py
+++ Team_M/backend/models/qdrant_models.py
@@ -5,8 +5,8 @@ class ProductRequest(BaseModel):
     name: Annotated[
         str,
         Field(
-            max_length=255,
-            description="Product name",
+            max_length=256,
+            description="Product Name",
             examples=[
                 "Organic Apple Juice",
                 "Great Value Whole Vitamin D Milk, Gallon, Plastic, Jug, 128 fl oz",
@@ -16,8 +16,8 @@ class ProductRequest(BaseModel):
     description: Annotated[
         str,
         Field(
-            max_length=50000,
-            description="Snippet content",
+            max_length=1024,
+            description="Product Description",
             examples=[
                 "Straight from the orchard, our organic apple juice is made from 100% pure pressed apples with no added sugars or preservatives.",
                 "Enjoy the wholesome goodness of Great Value Whole Vitamin D Milk Gallon in a plastic jug, 128 Fl Oz. This Grade A quality milk is pasteurized and delivers fresh from the farm taste. It offers an abundance of nutritional benefits such as protein, calcium, potassium and vitamins A and D. Our farms have pledged to not treat any cows with any artificial growth hormones. Great Value products provide families with affordable, high quality grocery options. With our wide range of product categories spanning grocery and household consumables, we offer you a variety of products for your family's needs. Our products are conveniently available online and in Walmart stores nationwide.",
